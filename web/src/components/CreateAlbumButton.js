09c6d1d10104a18501bd06523cc99aef37d73edc
Philipp Mudra <philipp.mudra@ise.de>
2019-09-03 16:16:22 +0200

    Add album button

--- web/src/components/CreateAlbumButton.js
+++ web/src/components/CreateAlbumButton.js
@@ -5,6 +5,19 @@ import { useMutation } from "react-apollo-hooks"
 import { BarLoader } from "react-spinners"
 import { Link } from "gatsby"
 
-// import { CREATE_ALBUM } from "../queries"
+import { CREATE_ALBUM } from "../queries"
+
+const CreateAlbumButton = ({ userId }) => {
+
+  const [createAlbum, { data, loading }] = useMutation(CREATE_ALBUM, {
+    variables: {
+      userId
+    }
+  })
+
+  console.log(data)
+
+  return <Button onClick={createAlbum}>Create Album</Button>
+}
 
 export default CreateAlbumButton
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -5,17 +5,19 @@ import { Heading, Button, Text } from "rebass"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-// import CreateAlbumButton from "../components/CreateAlbumButton"
+import CreateAlbumButton from "../components/CreateAlbumButton"
 import { useAuth } from "react-use-auth"
 
 const IndexPage = () => {
-  const { isAuthenticated, user, login } = useAuth()
+  const { isAuthenticated, user, login, logout } = useAuth()
   return (
     <Layout>
       <SEO title="Home" />
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Heading>Hi {isAuthenticated() ? user.nickname : null}</Heading>
-        {isAuthenticated() ? null : <Button onClick={login}>Login</Button>}
+        {!isAuthenticated() && <Button onClick={login}>Login</Button>}
+        {isAuthenticated() && <Button onClick={logout}>Logout</Button>}
+        {isAuthenticated() && <CreateAlbumButton userId={user.sub} />}
         <Image />
       </div>
     </Layout>
